81cfd2e9d3be46ad7583477c99bffb3e417598e0
vepifanio <vepifanio1@gmail.com>
2024-01-03 13:11:43 -0300

    refactor: Change construction for the MongoDB connection string

--- src/@types/process-env.d.ts
+++ src/@types/process-env.d.ts
@@ -1,7 +1,10 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     PORT: string | number
-    MONGO_URI: string
+    MONGO_INITDB_ROOT_USERNAME: string
+    MONGO_INITDB_ROOT_PASSWORD: string
+    MONGO_HOST: string
+    MONGO_DB_NAME: string
     BASE_URL: string
   }
 }
--- src/database/index.ts
+++ src/database/index.ts
@@ -5,9 +5,11 @@ dotenv.config({
   path: './env',
 })
 
+const mongoURI = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}?authSource=admin`
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {})
+    await mongoose.connect(mongoURI)
     console.log('Database connected')
   } catch (error) {
     console.error(error)
