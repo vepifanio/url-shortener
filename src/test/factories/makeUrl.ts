fe2319b2683f277cecde52c4c18fb49e10fda66f
vepifanio <vepifanio1@gmail.com>
2024-01-03 11:06:36 -0300

    test: Url factory for tests created

---
+++ src/test/factories/makeUrl.ts
@@ -0,0 +1,14 @@
+import { randomUUID } from 'node:crypto'
+import { Url, UrlProps } from '../../application/entity/Url'
+import { nanoid } from 'nanoid'
+
+export function makeUrl(props: Partial<UrlProps>) {
+  return Url.create({
+    id: randomUUID(),
+    clicks: 0,
+    createdAt: new Date(),
+    originalUrl: 'http://test.com',
+    shortUrlId: nanoid(),
+    ...props,
+  })
+}
